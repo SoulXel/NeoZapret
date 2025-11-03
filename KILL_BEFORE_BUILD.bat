85215631e5d78291c424122d28d1ef9415042923
Lu1ky <72498804+SoulXel@users.noreply.github.com>
2025-11-03 17:01:38 +0500

    Delete KILL_BEFORE_BUILD.bat

--- KILL_BEFORE_BUILD.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Остановка всех процессов NeoZapret...
-taskkill /F /IM NeoZapret.exe 2>nul
-taskkill /F /IM winws.exe 2>nul
-timeout /t 2 /nobreak >nul
-echo Процессы остановлены. Можно запускать сборку.
-pause
-
-
